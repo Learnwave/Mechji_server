6255d1d57bbf285f49951dd488bc2b972f1b9fcc
Learnwave <146640293+Learnwave@users.noreply.github.com>
2024-12-28 13:58:16 +0530

    admin controller function completed

--- Routes/adminRoute.js
+++ Routes/adminRoute.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { addShop } from '../controller/adminController.js';
+import { addShop,loginAdmin } from '../controller/adminController.js';
 import upload from '../Middleware/multer.js';
+import authAdmin from '../Middleware/authAdmin.js';
 
 const adminRouter = express.Router()
 
-adminRouter.post('/add-shop',upload.single('image'),addShop)
+adminRouter.post('/add-shop',authAdmin,upload.single('image'),addShop)
+adminRouter.post('/login',loginAdmin)
+
 
 export default adminRouter;
--- controller/adminController.js
+++ controller/adminController.js
@@ -2,6 +2,7 @@ import validator from 'validator'
 import bcrypt from "bcrypt"
 import { v2 as cloudinary } from 'cloudinary';
 import shopModel from '../models/shopModel.js';
+import jwt from 'jsonwebtoken'
 //api for adding shop
 
 const addShop = async (req,res) => {
@@ -51,11 +52,18 @@ const addShop = async (req,res) => {
             date:Date.now()
          }
 
-         const newShop = new shopModel(shopData)
+        try {
+            const newShop = new shopModel(shopData)
+            await newShop.save()
+            return res.json({success:true,message:"Shop Created"})
+            
+        } catch (error) {
+           return res.json({success:false,message:"Shop Already Registered"})
+        }
+         
+         
 
-         console.log(newShop)
-
-         return res.json({success:true,message:"Shop Created"})
+        
 
 
     } catch (error) {
@@ -63,4 +71,23 @@ const addShop = async (req,res) => {
         return res.json({success:false,message:error.message})
     }
 }
-export{addShop}
+
+//api for admin login
+
+const loginAdmin = async (req,res) => {
+    try {
+        
+        const {email,password} = req.body;
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email+password,process.env.JWT_SECRET);
+            res.json({success:true,token})
+        }else{
+            return res.json({success:false,message:"Invalid Credentials"})
+        }
+
+    } catch (error) {
+        return res.json({success:false,message:error.message})
+    }
+}
+
+export{addShop,loginAdmin}
