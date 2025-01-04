ad5b15063f173e0dabe924b764125e46f500bd83
Learnwave <146640293+Learnwave@users.noreply.github.com>
2025-01-04 09:32:31 +0530

    cors added

--- server.js
+++ server.js
@@ -16,15 +16,11 @@ const port = process.env.port || 4000;
 connectDB();
 connectCloudinary();
 
-const allowedOrigins = ["http://localhost:5173",]
-
 app.use(express.json());
 
 app.use(cookieParser());
 
-app.use(cors({origin:allowedOrigins,credentials: true}));
-
-
+app.use(cors({ origin: ["http://localhost:5173", "http://localhost:5174"],credentials:true }));
 // api endpoints
 
 app.use('/api/auth',authRouter);
